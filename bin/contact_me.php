1bab854cd588d3a1f3bd03bd8fc69b0b71234df8
Benjamin Levels <benlevels@gmail.com>
2017-04-07 21:31:52 -0700

    update links additions
    add contact form
    add privacy policy and mission statement
    reduce size of copyright and other footer elements

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -16,13 +16,13 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'benlevels@gmail.com'; // Add your email address inbetween the '' replacing            
-yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'info@spotlessmanagement.com'; // Add your email address inbetween the '' replacing            
+//yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Spotless Management, Inc. Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the        
 details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@spotlessmanagement.com\n"; // This is the email address the generated message will     
-be from. We recommend using something like noreply@yourdomain.com.
+//be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address"; 
 mail($to,$email_subject,$email_body,$headers);
 return true;            
