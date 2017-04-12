841deb52b0332c60b63afdc0d7b0589673c0e1bd
Benjamin Levels <benlevels@gmail.com>
2017-04-11 17:56:03 -0700

    update site colors
    model after GQ change contact form design
    add testimonial page
    change banner to feature about section

--- getting_started/assets/js/custom.js
+++ getting_started/assets/js/custom.js
@@ -53,7 +53,15 @@ $(document).ready(function() {
 
         }
     });
-	
+	 //Intialize Testamonials
+    var testimonials_slider = new Swiper('#demo-testimonial-1 .swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        parallax: true,
+        speed: 1000
+    });
     
 
 });
