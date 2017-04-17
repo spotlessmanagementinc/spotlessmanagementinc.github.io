cb63bc00d3f5936ebfdf100144b07dd9671f3241
Benjamin Levels <benlevels@gmail.com>
2017-04-16 23:36:45 -0700

    create landing page
    minimize content on page remove links for other pages, but save the
    pages under aliases

--- getting_started/assets/js/custom.js
+++ getting_started/assets/js/custom.js
@@ -70,7 +70,20 @@ $(document).ready(function() {
         speed: 1000
     });
   
-
+	$(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html, body').animate({
+			  scrollTop: target.offset().top
+			}, 1000);
+			return false;
+		  }
+		}
+	  });
+});
 
 
 
